9befbbf7fa78b86175544f6ba8012cf914dc87e2
mrmurtz <murtaza.s.abidi@gmail.com>
2016-09-09 14:27:32 +0100

    Completed 25, introduced partials, tests passsing

--- app/server.rb
+++ app/server.rb
@@ -2,10 +2,12 @@
 class BookmarkManager < Sinatra::Base
 
     enable :sessions
-    set :session_secret, 'super secret'
-    set :partial_template_engine, :erb
     register Sinatra::Flash
     register Sinatra::Partial
+    set :session_secret, 'super secret'
+    set :partial_template_engine, :erb
+
+    enable :partial_underscores
 
     get '/' do
       "welcome to book mark manager extraordinaire"
